0a9900122c85c7838e724250388fbe652ea2d40d
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-29 22:26:11 +0530

    connect to db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const flash = require('connect-flash');
-
+const db = require('./config/mongoose');
 const port = 8000;
 
 
