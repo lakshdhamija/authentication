fde4ca60b9603405423d6edfb9de027cf8ebf62e
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-06-29 22:20:56 +0530

    setup express-ejs-layouts

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const flash = require('connect-flash');
+
 const port = 8000;
+
+
+app.use(express.static('./assets'));
+
+
+
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 
 // set up the view engine
 app.set('view engine', 'ejs');
